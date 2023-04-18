6b386ccd0aba1f9f712e1499f2f5d778430465b2
ArdellNr <130547346+ArdellNr@users.noreply.github.com>
2023-04-18 17:20:15 +0700

    Add files via upload

---
+++ F01_F04.py
@@ -0,0 +1,118 @@
+import Utilities as u
+
+#F01 -- LogIn
+
+def logIn(dataUser : list, loggedIn : list):
+
+    usn = input("Masukkan Username: ")
+    pw = input("Masukkan password: ")
+
+    i = 0
+    if loggedIn != [None,None]:
+        while i < u.my_length(dataUser):
+            if dataUser[i][0] == usn: #ngecek dataUser
+                if dataUser[i][1] == pw:
+                    print("Selamat datang,"+usn+ "!")
+                    print("Masukkan command “help” untuk daftar command yang dapat kamu panggil.")
+                    loggedIn[0] = usn
+                    loggedIn[1] = dataUser[i][2]
+
+                    return loggedIn
+                else:
+                    print("Password salah!")
+                    return loggedIn
+            else: 
+                i += 1
+        print("Username tidak terdaftar!")
+        return loggedIn
+    else: 
+        print("Login gagal!")
+        print("Anda telah login dengan username Bandung, silahkan lakukan “logout” sebelum melakukan login kembali.")
+        return loggedIn
+    
+#F02 -- LogOut
+        
+def logout(loggedIn):
+    if loggedIn != [None,None]:
+        loggedIn = [None, None]
+    else:
+        print("Logout gagal!")
+        print("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout")
+
+#F03 -- SummonJin
+
+def summonJin(dataJin):
+
+    if u.my_length(dataJin) > 100:
+        print("Jumlah Jin telah maksimal! (100 jin). Bandung tidak dapat men-summon lebih dari itu")
+        return dataJin
+    else:
+        print("Jenis jin yang dapat dipanggil: ")
+        print(" (1) Pengumpul - Bertugas mengumpulkan bahan bangunan")
+        print(" (2) Pembangun - Bertugas membangun candi")
+        while True:
+            Jenis = input("Masukkan nomor jenis jin yang dipanggil: ")
+
+            if Jenis == 1:
+                print("Memilih jin “Pengumpul”.")
+                Jenis = "pengumpul"
+                break
+            elif Jenis == 2:
+                print("Memilih jin “Pembangun”.")
+                Jenis = "pembangun"
+                break
+            else: 
+                print(f"Tidak ada jenis jin bernomor “{Jenis}”!")
+    
+        i = 0
+        while True:
+            Jusn = str(input("Masukkan username jin: "))
+
+            if u.isAvailable(Jusn,dataJin):
+
+                while True:
+                    Jpw = str(input("Masukkan password jin: "))
+
+                    if u.my_length(Jpw) >= 5 and u.my_length(Jpw) <= 25:
+                        u.attach(dataJin, [Jusn, Jpw, Jenis])
+
+                        print("Mengumpulkan sesajen...")
+                        print("Menyerahkan sesajen...")
+                        print("Membacakan mantra...")
+                        print("")
+                        print("Jin Ïfrit berhasil dipanggil!")
+
+                        return dataJin
+                    
+                    else: 
+                        print("Password panjangnya harus 5-25 karakter!")
+            else:
+                print(f"Username “{Jusn}” sudah diambil!")
+
+
+#F04 -- HilangkanJin
+
+def hilangkanJin(dataJin):
+    hapus = input("Masukkan username jin : ")
+    for i in range (u.my_length(dataJin)):
+        if dataJin[i] == hapus:
+            ans = str(input("Apakah anda yakin ingin menghapus jin dengan username Jin1 (Y/N)? "))
+            if ans  == "Y" or ans == "y":
+                print("Jin telah berhasil dihapus dari alam gaib. ")
+                return u.my_delArray(dataJin, hapus)
+            else:
+                print("Tidak ada jin yang terhapus. ")
+                return dataJin
+    print("Tidak ada jin dengan username tersebut.")
+    return dataJin
+
+
+
+
+                
+
+
+
+
+
+
