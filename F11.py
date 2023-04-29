4bacf7ba47a1700dd3872f03988afde206a55e78
ArdellNr <130547346+ArdellNr@users.noreply.github.com>
2023-04-29 12:14:51 +0700

    Update F11.py

--- F11.py
+++ F11.py
@@ -5,18 +5,17 @@ from Utilities import *
 def HancurkanCandi (data_candi) :
     
     id = int(input("Masukkan ID candi : "))
-    count = 0
-    data_candi_temp = [[None for i in range (5)] for j in range(101)]
-    for i in range (0,100):
+    for i in range (my_length(data_candi)):
+        
         if data_candi[i][0]!=None and int(data_candi[i][0]) == id :
             jawab = str(input("Apakah anda yakin ingin menghancurkan candi ID(Y/N): "))
-            count = 1
+
             if jawab == 'Y' or jawab == 'y' :
                 data_candi = my_dellArray(data_candi, data_candi[i])
-                break
-    if count == 0 :
-        print("Tidak ada candi dengan  ID tersebut.")
-        return data_candi
+                return data_candi
+                
+     print("Tidak ada candi dengan  ID tersebut.")
+     return data_candi
     
 
 #HancurkanCandi()
