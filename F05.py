7f64ba420ff542a6cd4bfefc5472d9d1963248e9
TheoLN <theoliviusnatael@gmail.com>
2023-04-30 22:32:02 +0700

    update F05

--- F05.py
+++ F05.py
@@ -4,34 +4,38 @@ def ubahjin(dataJin):
     Jusn = str(input("Masukkan username jin: "))
     if Jusn == "Bondowoso" or Jusn =="Roro" :
         print("Tidak dapat diubah tipe")
-    elif u.isAvailable(Jusn,dataJin):
-        Jenis = None
-        index = None
-        for i in range(u.my_length(dataJin)):
-            if dataJin[i][0] == Jusn:
-                Jenis = dataJin[i][2]
-                index = i
-        if Jenis == "jin_pengumpul":
-            print("Jin ini bertipe pengumpul",end=" ")
-            Jenis_baru = input("Yakin ingin mengubah ke tipe “Pembangun” (Y/N)?")
-            if Jenis_baru == 'Y' or Jenis_baru =='y':
-                dataJin[index][2] = "jin_pembangun"
-                print("Jin telah berhasil diubah.")
+        return(dataJin)
+    
+  
+    Jenis = None
+    index = None
+    for i in range(u.my_length(dataJin)):
+        if dataJin[i][0] == Jusn:
+            Jenis = dataJin[i][2]
+            index = i
+            if Jenis == "jin_pengumpul":
+                print("Jin ini bertipe pengumpul",end=" ")
+                Jenis_baru = input("Yakin ingin mengubah ke tipe “Pembangun” (Y/N)?")
+
+                if Jenis_baru == 'Y' or Jenis_baru =='y':
+                    dataJin[index][2] = "jin_pembangun"
+                    print("Jin telah berhasil diubah.")
                 
-                return(dataJin)
+                    return(dataJin)
+                else:
+                    return(dataJin)
             else:
-                return(dataJin)
-        else:
-            print("Jin ini bertipe pembangun",end=" ")
-            Jenis_baru = input("Yakin ingin mengubah ke tipe “Pengumpul” (Y/N)?")
-            if Jenis_baru == 'Y' or Jenis_baru =='y':
-                dataJin[index][2] = "jin_pengumpul"
-                print("Jin telah berhasil diubah.")
-                return(dataJin)
-            else:
-                return(dataJin)
+                print("Jin ini bertipe pembangun",end=" ")
+                Jenis_baru = input("Yakin ingin mengubah ke tipe “Pengumpul” (Y/N)?")
+                if Jenis_baru == 'Y' or Jenis_baru =='y':
+                    dataJin[index][2] = "jin_pengumpul"
+                    print("Jin telah berhasil diubah.")
+                    return(dataJin)
+                else:
+                    return(dataJin)
     else:
         print("Tidak ada jin dengan username tersebut.")
+        return(dataJin)
         
 
 
